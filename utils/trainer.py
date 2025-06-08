b529160c061ae857b45f31525d262d775cef1fc8
Fazry <lhuqita.fazry@gmail.com>
2025-06-08 18:39:00 +0700

    Adding some scripts

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,3 +1,4 @@
+import os
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator
 
@@ -5,6 +6,6 @@ class Trainer(DefaultTrainer):
     @classmethod
     def build_evaluator(cls, cfg, dataset_name, output_folder=None):
         if output_folder is None:
-            os.makedirs("eval_output", exist_ok=True)
-            output_folder = "eval_output"
+            os.makedirs("./output/detection_train_eval_output", exist_ok=True)
+            output_folder = "./output/detection_train_eval_output"
         return COCOEvaluator(dataset_name, cfg, False, output_folder)
