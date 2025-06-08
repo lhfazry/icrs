9d6127d1b686df81163037757bef985934777749
Fazry <lhuqita.fazry@gmail.com>
2025-06-09 05:49:09 +0700

    add new script

--- run_download_dataset.sh
+++ run_download_dataset.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+mkdir -p data/detection
+
 gdown 1I6WY21iCs_XR8wFWM-79JRryp_9PunMT
 mv input_video.mp4 data
 
 gdown 1V3jDKzgbIKk1fLTpYWScM9O2h2o9pPh1
-mkdir -p data/detection
 unzip icrs.zip -d data/detection > /dev/null
