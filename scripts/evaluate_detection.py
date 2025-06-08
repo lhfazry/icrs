3e1a2de1c3d26d7ec88ee71df1741ceb1e2e6a16
Fazry <lhuqita.fazry@gmail.com>
2025-06-08 19:58:46 +0700

    Fixing some bugs

--- scripts/evaluate_detection.py
+++ scripts/evaluate_detection.py
@@ -1,20 +1,22 @@
+import os
+import torch
+
 from detectron2.engine import DefaultPredictor
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.data import build_detection_test_loader
 from scripts.train_detection import register_datasets
 from detectron2.config import get_cfg
 from utils.path_util import ensure_root
-from config import DATASET_NAME, DETECTION_OUTPUT_DIR, DETECTION_EVAL_OUTPUT_DIR, NUM_CLASSES, DETECTION_THRESHOLD
-import os
+from config import DATASET_NAME, DETECTION_OUTPUT_DIR, DETECTION_EVAL_OUTPUT_DIR, DETECTION_THRESHOLD
 
 def evaluate():
     register_datasets()
     
-    cfg = get_cfg()
-    cfg.merge_from_file(os.path.join(DETECTION_OUTPUT_DIR, "detection_best_model_config.pth"))
+    #cfg = get_cfg()
+    #cfg.merge_from_file(os.path.join(DETECTION_OUTPUT_DIR, "detection_best_model_config.pth"))
+    cfg = torch.load(os.path.join(DETECTION_OUTPUT_DIR, "detection_best_model_config.pth"))
     cfg.MODEL.WEIGHTS = os.path.join(DETECTION_OUTPUT_DIR, "detection_best_model.pth")
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = DETECTION_THRESHOLD
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = NUM_CLASSES
     predictor = DefaultPredictor(cfg)
     
     evaluator = COCOEvaluator(f"{DATASET_NAME}_test", output_dir=DETECTION_EVAL_OUTPUT_DIR)
